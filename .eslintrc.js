5dc4687bf2e42f39ea73b7c09438fe79b37d0c28
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-02 17:36:27 -0300

    ➕ chore: adiciona dependencias
    -cryptojs
    -express e express aasync errors
    - joi
    -mysql2
    -squelize e sequelize cli
    -shelljs
    -eslint
    -nodemon

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
