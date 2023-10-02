18fbc48e67d1117f09777877c13b32fdf31c751a
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-02 20:46:19 -0300

    🎨 feat: adiciona barrel para middlewares

---
+++ src/middlewares/index.js
@@ -0,0 +1,7 @@
+const validateAccessToken = require('./validateAccessToken');
+const validateLoginBody = require('./validateLoginBody');
+
+module.exports = {
+  validateAccessToken,
+  validateLoginBody,
+};
